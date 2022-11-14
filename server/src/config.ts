2504651187c837538338b5181c4b1a665334bb67
x1kk4 <grigory.valevin@gmail.com>
2022-11-15 01:54:44 +0300

    some minor stylistic changes

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,7 +1,56 @@
-export function startApp(app: any, PORT: string) {
+import codes from "http-status-codes";
+import express from "express";
+import dotenv from "dotenv";
+import { Http, HttpRequestOptions, Method } from "node-https";
+import { updateNewsList } from "./newsService";
+import { NewsApiResponse, NewsComponentId } from "./types";
+dotenv.config();
+
+let temp: NewsComponentId[] = [];
+
+const http = new Http();
+
+const API = process.env.API_HOST;
+const DEFAULT_LIST_AMOUNT = parseInt(process.env.DEFAULT_LIST_AMOUNT as string);
+
+const app = express();
+
+app.get("/news", async (req: express.Request, res: express.Response) => {
   try {
-    app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+    const result: NewsApiResponse = (await http.get(`${API}`)) as any;
+
+    if (!result?.data) {
+      throw new Error("API is not currently available");
+    }
+
+    temp = result.data.slice(0, DEFAULT_LIST_AMOUNT);
+
+    res.status(codes.OK).json(result.data.slice(0, DEFAULT_LIST_AMOUNT));
   } catch (e) {
-    console.log(e);
+    console.error(`Error when getting news: ${e}`);
+
+    res.status(codes.BAD_REQUEST).json(e);
   }
-}
+});
+
+app.get("/news/update", async (req: express.Request, res: express.Response) => {
+  try {
+    const result: NewsApiResponse = (await http.get(`${API}`)) as any;
+
+    if (!result?.data) {
+      throw new Error("API is not currently available");
+    }
+
+    const curr = result.data ? result.data.slice(0, DEFAULT_LIST_AMOUNT) : [];
+    const output = updateNewsList(temp, curr);
+    temp = output.temp;
+
+    res.status(codes.OK).json(output.items);
+  } catch (e) {
+    console.error(`Error when updating news: ${e}`);
+
+    res.status(codes.BAD_REQUEST).json(e);
+  }
+});
+
+export default app;
--- server/src/newsService.ts
+++ server/src/newsService.ts
@@ -1,16 +1,21 @@
-export const updateNewsList = (temporary: Number[], current: Number[]) => {
-  let additionalItems: Number[] = [];
+import { NewsComponentId } from "./types";
+
+export const updateNewsList = (
+  temporary: NewsComponentId[],
+  current: NewsComponentId[]
+): { temp: NewsComponentId[]; items: NewsComponentId[] } => {
+  let additionalItems: NewsComponentId[] = [];
+
   if (temporary == current) {
     return { temp: temporary, items: additionalItems };
   } else if (current.includes(temporary[0])) {
     let mark = temporary[0];
+
     let index = current.indexOf(mark);
+
     temporary = temporary.slice(0, temporary.length - index);
     additionalItems = current.slice(0, index);
     temporary.unshift(...current.slice(0, index));
-  } else {
-    console.log("123");
   }
-
   return { temp: temporary, items: additionalItems };
 };
